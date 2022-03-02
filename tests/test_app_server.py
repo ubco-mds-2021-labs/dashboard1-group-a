bce9f256a7363df1f28f288e1e709f68301b49b8
Gavin Grochowski <grochowskigavin@gmail.com>
2022-03-02 14:18:41 -0800

    Fix run_server test with proper id outside of debug mode

--- tests/test_app_server.py
+++ tests/test_app_server.py
@@ -7,9 +7,9 @@ def test_dbap001_run_server(dash_duo):
     app = import_app("server")
     dash_duo.start_server(app)
 
-    # Ensure that the dash app runs by waiting until the "_dash-app-content"
-    # section loads (the html element with our app content), or 30 seconds.
-    dash_duo.wait_for_element_by_id("_dash-app-content", timeout=30)
+    # TODO: Ensure that the dash app runs by waiting until the "_dash-app-content"
+    # section loads (the html element with our app content), or 10 seconds.
+    dash_duo.wait_for_element_by_id("react-entry-point", timeout=10)
 
     # Ensure no errors occured in the browser.
     assert dash_duo.get_logs() == [], "Browser console should contain no error"

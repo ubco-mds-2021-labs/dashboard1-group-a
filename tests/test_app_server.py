53817f2d2cf7c3deeeda3797ed9d5e70cdc9436d
Gavin Grochowski <grochowskigavin@gmail.com>
2022-03-02 10:21:25 -0800

    Add test to ensure that the app runs without error

--- tests/test_app_server.py
+++ tests/test_app_server.py
@@ -1,2 +1,15 @@
-def test_placeholder():
-    pass
+from dash.testing.application_runners import import_app
+from dash.testing.composite import DashComposite
+
+
+def test_dbap001_run_server(dash_duo):
+    # Start dash app with the "app" variable from server.py
+    app = import_app("server")
+    dash_duo.start_server(app)
+
+    # Ensure that the dash app runs by waiting until the "_dash-app-content"
+    # section loads (the html element with our app content), or 30 seconds.
+    dash_duo.wait_for_element_by_id("_dash-app-content", timeout=30)
+
+    # Ensure no errors occured in the browser.
+    assert dash_duo.get_logs() == [], "Browser console should contain no error"

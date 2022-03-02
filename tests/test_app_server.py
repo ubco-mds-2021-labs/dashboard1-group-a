ed7fded215cdc69d885af8c5d833c1dd0b7e748b
Gavin Grochowski <grochowskigavin@gmail.com>
2022-03-02 14:22:30 -0800

    Adjust code comment to reflect new id

--- tests/test_app_server.py
+++ tests/test_app_server.py
@@ -7,7 +7,7 @@ def test_dbap001_run_server(dash_duo):
     app = import_app("server")
     dash_duo.start_server(app)
 
-    # TODO: Ensure that the dash app runs by waiting until the "_dash-app-content"
+    # Ensure that the dash app runs by waiting until the "react-entry-point"
     # section loads (the html element with our app content), or 10 seconds.
     dash_duo.wait_for_element_by_id("react-entry-point", timeout=10)
 

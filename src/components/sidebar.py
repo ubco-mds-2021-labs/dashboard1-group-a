60cb2cc70c502ecfebd0ed1360c66b893492eb7d
ysun822 <sunyilin@seas.upenn.edu>
2022-03-02 19:54:25 -0800

    add the callback of the dropdown

--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -1,8 +1,9 @@
+import dash
 from dash import dcc
-from dash import html
+from dash import html,Input, Output
 import dash_bootstrap_components as dbc
-
-from .sidebar_function_tab1 import TAB1_DROPDOWN,room_selector,time_scale,date_slider
+from ..app import app
+from .sidebar_function_tab1 import TAB1_DROPDOWN,time_scale,date_slider,choose_fun
 
 
 SIDEBAR = dbc.Col(md=3, style={'backgroundColor':'black','color':'white'},children=[dbc.Row("Energy Dashboard",style={"font-size":"30px"}),
@@ -11,12 +12,12 @@ dbc.Row("Add description here"),
 
 dbc.Label("Compare Across:",style={"font-size":"20px"}),
 dbc.Row(TAB1_DROPDOWN),
-
-dbc.Row(room_selector),
+dbc.Row(id="selection"),
 dbc.Row(time_scale),
 dbc.Row(date_slider),
-
-
-
 ],
 )
+
+@app.callback(Output("selection", "children"), [Input("tab1-dropdown", "value")])
+def output_div(value):
+    return choose_fun(value)
--- src/components/sidebar_function_tab1.py
+++ src/components/sidebar_function_tab1.py
@@ -3,6 +3,18 @@ from dash import Input, Output,html
 import dash_bootstrap_components as dbc
 from ..app import app
 
+
+def choose_fun(fun=1):
+    if fun == 1:
+        return room_selector
+    if fun == 2:
+        return sun_light_selector
+    if fun == 3:
+        return floor_selector
+    if fun == 4:
+        return day_night_selector
+
+
 TAB1_DROPDOWN = dcc.Dropdown(
     id="tab1-dropdown",
     options=[
@@ -26,6 +38,57 @@ room_selector = html.Div(
                 {"label": "Kitchen", "value": "kich"}
             ],
             value="bed",
+            inline=False,
+        ),
+    ],
+    className="mb-2",
+)
+
+sun_light_selector = html.Div(
+    [
+        dbc.Label("Include:"),
+        dbc.RadioItems(
+            id="sun_light_selector",
+            options=[
+                {"label": "East", "value": "east"},
+                {"label": "West", "value": "west"}
+
+            ],
+            value="east",
+            inline=False,
+        ),
+    ],
+    className="mb-2",
+)
+
+floor_selector = html.Div(
+    [
+        dbc.Label("Include:"),
+        dbc.RadioItems(
+            id="floor_selector",
+            options=[
+                {"label": "Ground", "value": "ground"},
+                {"label": "Second", "value": "second"}
+
+            ],
+            value="ground",
+            inline=False,
+        ),
+    ],
+    className="mb-2",
+)
+
+day_night_selector = html.Div(
+    [
+        dbc.Label("Include:"),
+        dbc.RadioItems(
+            id="day_night_selector",
+            options=[
+                {"label": "Day", "value": "day"},
+                {"label": "Night", "value": "night"}
+
+            ],
+            value="day",
             inline=False,
         ),
     ],

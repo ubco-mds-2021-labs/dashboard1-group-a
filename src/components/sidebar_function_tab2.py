392343934767573198d4c3a42fa4a062db79bc00
ysun822 <sunyilin@seas.upenn.edu>
2022-03-09 15:06:33 -0800

    edit sidebar2

--- src/components/sidebar_function_tab2.py
+++ src/components/sidebar_function_tab2.py
@@ -4,14 +4,6 @@ import dash_bootstrap_components as dbc
 from ..app import app
 
 
-def choose_fun_tab2(fun=1):
-    if fun == 1:
-        return humidity_selector
-    if fun == 2:
-        return temperature_selector
-    if fun == 3:
-        return wind_selector
-
 
 TAB2_DROPDOWN = dcc.Dropdown(
     id="tab2_dropdown",
@@ -24,62 +16,80 @@ TAB2_DROPDOWN = dcc.Dropdown(
 
 )
 
-humidity_selector = html.Div(
+tab2_selector = html.Div(
     [
-        dbc.Label("Include:",class_name="sub_title"),
+        dbc.Label("Include:",class_name="sub_title_2"),
         dbc.Checklist(
-            id="humidity_selector",
+            id="selection_tab2",
             options=[
-                {"label": "Low Humidity", "value": "low_hum"},
-                {"label": "Mid-low Humidity", "value": "mid_low_hum"},
-                {"label": "Mid-high Humidity", "value": "mid_high_hum"},
-                {"label": "High Humidity", "value": "high_hum"}
-
+                {"label": "Low Humidity", "value": "Low Outside Humidity"},
+                {"label": "Mid-low Humidity", "value": "Mid-Low Outside Humidity"},
+                {"label": "Mid-high Humidity", "value": "Mid-High Outside Humidity"},
+                {"label": "High Humidity", "value": "High Outside Humidity"}
             ],
+            value=['Functional Space'],
 
             inline=False,
         ),
     ],
     className="mb-2",
 )
-
-temperature_selector = html.Div(
-    [
-        dbc.Label("Include:",class_name="sub_title"),
-        dbc.Checklist(
-            id="temperature_selector",
-            options=[
-            {"label": "Low Outside Temperature", "value": "low_tem"},
-            {"label": "Mid-low Outside Temperature", "value": "mid_low_tem"},
-            {"label": "Mid-high Outside Temperature", "value": "mid_high_tem"},
-            {"label": "High Outside Temperature", "value": "high_tem"}
-
-
-            ],
-
-            inline=False,
-        ),
-    ],
-    className="mb-2",
-)
-
-wind_selector = html.Div(
-    [
-        dbc.Label("Include:",class_name="sub_title"),
-        dbc.Checklist(
-            id="wind_selector",
-            options=[
-            {"label": "Low Windspeed", "value": "low_wind"},
-            {"label": "Mid-low Windspeed", "value": "mid_low_wind"},
-            {"label": "Mid-high Windspeed", "value": "mid_high_wind"},
-            {"label": "High Windspeed", "value": "high_wind"}
-
-
-
-            ],
-
-            inline=False,
-        ),
-    ],
-    className="mb-2",
-)
+# humidity_selector = html.Div(
+#     [
+#         dbc.Label("Include:",class_name="sub_title"),
+#         dbc.Checklist(
+#             id="humidity_selector",
+#             options=[
+#                 {"label": "Low Humidity", "value": "low_hum"},
+#                 {"label": "Mid-low Humidity", "value": "mid_low_hum"},
+#                 {"label": "Mid-high Humidity", "value": "mid_high_hum"},
+#                 {"label": "High Humidity", "value": "high_hum"}
+#
+#             ],
+#
+#             inline=False,
+#         ),
+#     ],
+#     className="mb-2",
+# )
+#
+# temperature_selector = html.Div(
+#     [
+#         dbc.Label("Include:",class_name="sub_title"),
+#         dbc.Checklist(
+#             id="temperature_selector",
+#             options=[
+#             {"label": "Low Outside Temperature", "value": "low_tem"},
+#             {"label": "Mid-low Outside Temperature", "value": "mid_low_tem"},
+#             {"label": "Mid-high Outside Temperature", "value": "mid_high_tem"},
+#             {"label": "High Outside Temperature", "value": "high_tem"}
+#
+#
+#             ],
+#
+#             inline=False,
+#         ),
+#     ],
+#     className="mb-2",
+# )
+#
+# wind_selector = html.Div(
+#     [
+#         dbc.Label("Include:",class_name="sub_title"),
+#         dbc.Checklist(
+#             id="wind_selector",
+#             options=[
+#             {"label": "Low Windspeed", "value": "low_wind"},
+#             {"label": "Mid-low Windspeed", "value": "mid_low_wind"},
+#             {"label": "Mid-high Windspeed", "value": "mid_high_wind"},
+#             {"label": "High Windspeed", "value": "high_wind"}
+#
+#
+#
+#             ],
+#
+#             inline=False,
+#         ),
+#     ],
+#     className="mb-2",
+# )

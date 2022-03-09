fcaf41369a0245d66052548a8b6ffdde6ccc1ba4
Kristy Phipps <phipps.kristy@gmail.com>
2022-03-08 19:16:40 -0800

    plot 2 dropdown/callback for y values added

--- src/components/tab2.py
+++ src/components/tab2.py
@@ -1,6 +1,7 @@
 from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
 from data.data import energy_df_full
 import altair as alt
@@ -23,7 +24,7 @@ def energy_plot():
         color = "total:N").properties(height=300,width=500)
     return chart1a.to_html()
 
-def weather_plot():
+def weather_plot(ycol="temperature_outside"):
     start_date = "2016-01-12"
     end_date = "2016-01-13"
     mask = (energy_df_full["date"] > start_date) & (energy_df_full["date"] <= end_date)
@@ -31,15 +32,26 @@ def weather_plot():
 
     chart1b = alt.Chart(single_day).mark_line().encode(
         alt.X("date:T", axis=alt.Axis(title='Elapsed Time')),
-        alt.Y("temperature_outside")).properties(height=200, width=500)
+        y=ycol).properties(height=200, width=500)
     return chart1b.to_html()
 
 plot1a = html.Iframe(srcDoc=energy_plot(),style={'border-width':'0','width':'100%', 'height': '400px'})
-plot1b = html.Iframe(srcDoc=weather_plot(),style={'border-width':'0','width':'100%', 'height': '400px'})   
+plot1b = html.Iframe(id='plot1b',srcDoc=weather_plot(ycol="temperature_outside"),style={'border-width':'0','width':'100%', 'height': '400px'})   
+
+
+weather_list = ["temperature_outside","dewpoint","humidity_outside","pressure","windspeed","visibility"] 
+choice = dcc.Dropdown(id='chart_dropdown',value='temperature_outside', options = [{'label': i, 'value': i} for i in weather_list])
 
 
 TAB2 = dbc.Tab(label="Energy Usage", children=["Tab 2 Content",
-                                                plot1a, plot1b])
+                                                plot1a, choice, plot1b,])
+
+## Callback functions
+@app.callback(
+    Output('plot1b','srcDoc'), # Specifies where the output of plot_weather() "goes"
+    Input('chart_dropdown', 'value'))
+def update_plot(ycol):
+    return weather_plot(ycol)
 
 if __name__ == '__main__':
     app.run_server(debug=True)

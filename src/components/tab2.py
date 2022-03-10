8996a8c16eb606e3bd96cea7cb4ecaf522f3b52e
Gavin Grochowski <grochowskigavin@gmail.com>
2022-03-10 15:17:53 -0800

    Add in column filtering during tab 2 visualizations

--- src/components/tab2.py
+++ src/components/tab2.py
@@ -12,13 +12,20 @@ alt.data_transformers.disable_max_rows()
 
 # Plots
 def energy_plot():
+    # Filtering values
+    necessary_cols = ["date", "energy_appliances", "energy_lights"]
     start_date = "2016-01-12"
     end_date = "2016-01-13"
-    mask = (energy_df_full["date"] > start_date) & (energy_df_full["date"] <= end_date)
-    single_day = energy_df_full[mask]
+
+    # Apply Filters
+    energy_df_filtered = energy_df_full[necessary_cols]
+    mask = (energy_df_filtered["date"] > start_date) & (
+        energy_df_filtered["date"] <= end_date
+    )
+    energy_df_filtered = energy_df_filtered[mask]
 
     chart1a = (
-        alt.Chart(single_day)
+        alt.Chart(energy_df_filtered)
         .transform_fold(["energy_appliances", "energy_lights"], as_=["total", "value"])
         .mark_bar()
         .encode(
@@ -32,13 +39,21 @@ def energy_plot():
 
 
 def weather_plot(ycol="temperature_outside"):
+    # Filtering values
+    necessary_cols = ["date"]
+    necessary_cols.append(ycol)
     start_date = "2016-01-12"
     end_date = "2016-01-13"
-    mask = (energy_df_full["date"] > start_date) & (energy_df_full["date"] <= end_date)
-    single_day = energy_df_full[mask]
+
+    # Apply Filters
+    energy_df_filtered = energy_df_full[necessary_cols]
+    mask = (energy_df_filtered["date"] > start_date) & (
+        energy_df_filtered["date"] <= end_date
+    )
+    energy_df_filtered = energy_df_full[mask]
 
     chart1b = (
-        alt.Chart(single_day)
+        alt.Chart(energy_df_filtered)
         .mark_line()
         .encode(alt.X("date:T", axis=alt.Axis(title="Elapsed Time")), y=ycol)
         .properties(height=200, width=500)
@@ -55,9 +70,6 @@ plot1b = html.Iframe(
     srcDoc=weather_plot(ycol="temperature_outside"),
     style={"border-width": "0", "width": "100%", "height": "400px"},
 )
-
-
-
 
 
 TAB2 = dbc.Tab(

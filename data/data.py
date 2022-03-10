4059c434280113f45f9507411d4dd01ecb8138f7
Kristy Phipps <phipps.kristy@gmail.com>
2022-03-10 15:01:23 -0800

    Limiting columns on dfs

--- data/data.py
+++ data/data.py
@@ -1,12 +1,13 @@
 import pandas as pd
+import numpy as np
 
 TEMP_DATA_PATH = "data/temperature_df_full.csv"
 ENERGY_DATA_PATH = "data/energy_df_full.csv"
 
 # Temperature dataframe for Tab 1 Use
-temperature_df_full = pd.read_csv("data/temperature_df_full.csv").astype(
-    {"date": "datetime64"}
-)
+temperature_df_full = pd.read_csv("data/temperature_df_full.csv").astype({"date": "datetime64"})
+temperature_df_full = temperature_df_full.iloc[:, np.r_[1,8:19]] #reduce number of columns being imported into tab1
 
 # Energy dataframe for Tab 2 Use
 energy_df_full = pd.read_csv("data/energy_df_full.csv").astype({"date": "datetime64"})
+energy_df_full = energy_df_full.iloc[:, np.r_[1,2,3, 22:36]] #reduce number of columns being imported into tab2

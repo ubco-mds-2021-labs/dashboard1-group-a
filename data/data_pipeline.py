12ed34836f0e70b40b944977fa99b57578e03c00
Firas Moosvi <firas@moosvi.com>
2022-03-10 13:19:01 -0800

    reduce files more?

--- data/data_pipeline.py
+++ data/data_pipeline.py
@@ -22,7 +22,7 @@ temperature_df_full = (
     .pipe(add_date_features)
     .pipe(pivot_longer_by_humidity_and_temperature)
     .pipe(add_location_features)
-)
+).iloc[0:5000]
 
 # Energy dataframe for Tab 2 Use
 energy_df_full = (
@@ -31,7 +31,7 @@ energy_df_full = (
     .pipe(drop_and_rename_columns)
     .pipe(add_date_features)
     .pipe(add_features_for_energy_comparison)
-)
+).iloc[0:5000]
 
 # Write to .csv file to save space in memory on the heroku app.
 if __name__ == "__main__":
